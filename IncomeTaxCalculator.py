2df3de91f3409b54b15e894fc8a9efb79f259b5d
estuardomendez001 <115508509+estuardomendez001@users.noreply.github.com>
2022-10-17 18:47:38 -0400

    Add files via upload

--- IncomeTaxCalculator.py
+++ IncomeTaxCalculator.py
@@ -1,7 +1,7 @@
 
 
-gross_income = float(input())   #gross income
-num_dependents = int(input())   #number of dependents
+gross_income = 150000.00   #gross income
+num_dependents = 3   #number of dependents
 standard_deduction = 10000
 
 
@@ -9,7 +9,7 @@ print("Enter the gross income: ", f'{gross_income:.2f}')
 print("Enter the number of dependents: ", num_dependents)
 
 
-
+#sum of gross income after deductions
 sum = (gross_income - standard_deduction - (num_dependents * 3000))
 
 
